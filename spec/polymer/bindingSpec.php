b6a8e4d51fe57bea07fc110e4a2aa5fec8dae8bc
Warren Seymour <wms@fountainhead-solutions.net>
2014-06-10 11:31:21 +0100

    ♡ Kahlan

--- spec/polymer/bindingSpec.php
+++ spec/polymer/bindingSpec.php
@@ -27,15 +27,11 @@ describe("binding", function() {
 	describe("li3 adapter", function() {
 		it("should call the correct class, model and parameters", function() {
 			$binding = new \polymer\data\binding\Li3();
-			$model = '\spec\polymer\mock\Li3Model';
+			$model = 'spec\polymer\mock\Li3Model';
 			$conditions = [ 'group' => 123 ];
 
+			expect($model)->toReceive('::all')->with(compact('conditions'));
 			$binding->apply($model, 'all', compact('conditions'));
-			expect($model::$stack)->toHaveLength(1);
-			expect($model::$stack[0])->toEqual([
-				'method' => 'all',
-				'options' => compact('conditions')
-			]);
 		});
 	});
 });
--- spec/polymer/mock/Li3Model.php
+++ spec/polymer/mock/Li3Model.php
@@ -4,14 +4,7 @@ namespace spec\polymer\mock;
 
 class Li3Model extends \lithium\data\Model {
 
-	public static $stack = [];
-
 	public static function all(array $options = []) {
-		static::$stack[] = [
-			'method' => 'all',
-			'options' => $options
-		];
-
 		return null;
 	}
 }
